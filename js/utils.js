608d9ebada2df80d758b4eada1c73a83b63e3491
makoban <152707033+makoban@users.noreply.github.com>
2025-08-06 14:33:58 +0900

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -3,7 +3,7 @@
 // ★★★ ここをあなたの情報に置き換えてください ★★★
 const API_KEY = 'AIzaSyDUFH1jbIa66NjzSM8I9Xpicwo6I6H2sB8';             
 const INITIAL_PUBLIC_CALENDAR_ID = 'kaigishitsu20000101@gmail.com'; 
-const CLIENT_ID = '155458326593-redrcpnqa2iqapvg8p1kb3cgegeenvse.apps.googleusercontent.com'; 
+const CLIENT_ID = '155458326593-g92losmr00mjj7g4sung2k8kjv9d01b.apps.googleusercontent.com';
 // ★★★ ここまで ★★★
 
 const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest"];
